1311af3d0bf7b45a0cf354af252297ffff78452e
Arpit472002 <sarpit623@gmail.com>
2022-12-07 22:34:30 +0530

    New Commit

--- employees/views.py
+++ employees/views.py
@@ -2,7 +2,9 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from employees.forms import EmployeeForm
 from employees.models import EmployeeList
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url='/login')
 def home(request):
     if request.method=='GET':
         form=EmployeeForm()
@@ -13,15 +15,17 @@ def home(request):
         if form.is_valid():
             form.save()
         list=EmployeeList.objects.all()
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/home')
 
+@login_required(login_url='/login')
 def delete(request,id):
     if request.method=='POST':
         x=EmployeeList.objects.get(id=id)
         x.delete()
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/home')
     return HttpResponse("Item not deleted")
 
+@login_required(login_url='/login')
 def update(request,id):
     if request.method=='GET':
         x=EmployeeList.objects.get(id=id)
@@ -32,4 +36,4 @@ def update(request,id):
         form=EmployeeForm(request.POST,request.FILES,instance=x)
         if form.is_valid():
             form.save()
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/home')
